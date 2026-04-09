d8fe3c437fa8844b74e09abaf00ee38fb64a7f8e
meorphis <eric@stainless.com>
2026-04-09 11:33:37 -0400

    Generate SDK

--- src/core/api-promise.ts
+++ src/core/api-promise.ts
@@ -1,6 +1,6 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-import { type Stainless12 } from '../client';
+import { type Stainless13 } from '../client';
 
 import { type PromiseOrValue } from '../internal/types';
 import { APIResponseProps, defaultParseResponse } from '../internal/parse';
@@ -11,13 +11,13 @@ import { APIResponseProps, defaultParseResponse } from '../internal/parse';
  */
 export class APIPromise<T> extends Promise<T> {
   private parsedPromise: Promise<T> | undefined;
-  #client: Stainless12;
+  #client: Stainless13;
 
   constructor(
-    client: Stainless12,
+    client: Stainless13,
     private responsePromise: Promise<APIResponseProps>,
     private parseResponse: (
-      client: Stainless12,
+      client: Stainless13,
       props: APIResponseProps,
     ) => PromiseOrValue<T> = defaultParseResponse,
   ) {
--- tests/api-resources/orgs.test.ts
+++ tests/api-resources/orgs.test.ts
@@ -1,8 +1,8 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-import Stainless12 from '@stainless-api/sdk';
+import Stainless13 from '@stainless-api/sdk';
 
-const client = new Stainless12({
+const client = new Stainless13({
   apiKey: 'My API Key',
   baseURL: process.env['TEST_API_BASE_URL'] ?? 'http://127.0.0.1:4010',
 });
